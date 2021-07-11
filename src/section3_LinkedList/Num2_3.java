d4e49c762e8ff44d66964bc6eb92ea76d9a7b3e2
Gunwoo Park <prayzz12@gmail.com>
2021-07-11 20:30:50 +0900

    02. 연결리스트 3번까지

--- src/section3_LinkedList/Num2_3.java
+++ src/section3_LinkedList/Num2_3.java
@@ -1,5 +1,8 @@
 package section3_LinkedList;
 
+import java.util.LinkedList;
+import org.junit.jupiter.api.Test;
+
 /**
  * title : 중간 노드 삭제
  * content : 단방향 연결리스트가 주어졌을 때 중간
@@ -9,7 +12,28 @@ package section3_LinkedList;
  */
 public class Num2_3 {
 
-    public void removeMidElement(){
+    /**
+     * 처음과 끝 노드가 아니기 때문에
+     * list.size() > 2 라는 조건이라고 가정한다.
+     * list.size()를 2로 나눈 값이 중간 값이다.
+     * @param list
+     */
+    public void removeMidElement(LinkedList<Object> list){
+        int mid = list.size() / 2;
+        list.remove(mid);
+        for (Object o : list) {
+            System.out.println(o.toString());
+        }
+    }
 
+    @Test
+    void 중간_노드_삭제(){
+        LinkedList<Object> list = new LinkedList<>();
+        list.add("e");
+        list.add("d");
+        list.add("c");
+        list.add("b");
+        list.add("a");
+        removeMidElement(list);
     }
 }
